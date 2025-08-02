7bd2fefc63f63484596169236f25f953fc8e8c0e
tuantm90 <tuantm90+github@hotmail.com>
2025-08-02 22:21:45 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,10 +17,11 @@ sed -i "s/set system\.@system\[-1\]\.hostname='[^']*'/set system.@system[-1].hos
 # ✅ Đổi timezone thành Asia/Ho_Chi_Minh
 sed -i "s/set system\.@system\[-1\]\.timezone='[^']*'/set system.@system[-1].timezone='Asia\/Ho_Chi_Minh'/" "$CFG_FILE"
 
-# ✅ Đổi danh sách NTP server
-# Xóa toàn bộ dòng add_list system.ntp.server hiện tại và thêm mới
-sed -i "/add_list system.ntp.server/d" "$CFG_FILE"
-sed -i "/set system.ntp.enable_server='0'/a \
+# ✅ Xoá tất cả các dòng add_list ntp cũ
+sed -i "/add_list system\.ntp\.server=/d" "$CFG_FILE"
+
+# ✅ Thêm các dòng add_list mới sau dòng set system.ntp.enable_server='0'
+sed -i "/set system\.ntp\.enable_server='0'/a \
 add_list system.ntp.server='vn.pool.ntp.org'\n\
 add_list system.ntp.server='time.google.com'\n\
 add_list system.ntp.server='time.cloudflare.com'\n\
